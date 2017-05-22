83dbf2a0d52e75cc8a5f0038887cc7a8d6bef01b
domkluz <kluz.dominic@gmail.com>
2017-05-22 07:31:01 +0000

    22.05 5:30pm

--- blog/config.py
+++ blog/config.py
@@ -5,4 +5,10 @@ class DevelopmentConfig(object):
     DEBUG = True
     SECRET_KEY = os.environ.get("BLOGFUL_SECRET_KEY", os.urandom(12))
     
+
+class TestingConfig(object):
+    SQLALCHEMY_DATABASE_URI = "postgresql://ubuntu:thinkful@localhost:5432/blogful-test"
+    DEBUG = True
+    SECRET_KEY = "Not secret"
     
+
